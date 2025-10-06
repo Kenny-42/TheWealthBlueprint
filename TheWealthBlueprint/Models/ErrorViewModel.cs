c5ab84c3446734c1a27d18a750dda75e45c53de9
Kendall Dawson <lilkenny642@gmail.com>
2025-10-05 22:23:59 -0700

    Add project files.

---
+++ TheWealthBlueprint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheWealthBlueprint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
