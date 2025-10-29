69f1901af10ac56f4694c13e040784c7b4fd9129
Kenny-42 <lilkenny642@gmail.com>
2025-10-28 22:17:35 -0700

    Customize ApplicationUser with FirstName and LastName
    Added FirstName and LastName properties to the ApplicationUser entity with required constraints and a maximum length of 50 characters. Updated the ApplicationDbContext to configure these properties in the OnModelCreating method.
    
    Created migrations to add and enforce these changes in the database schema. Updated the ApplicationDbContextModelSnapshot and migration designer files to reflect the new entity structure. Refactored identity relationships to use the customized ApplicationUser class.
    
    Improved code by replacing deprecated methods and updating annotations for identity-related entities.

--- TheWealthBlueprint/Data/ApplicationDbContext.cs
+++ TheWealthBlueprint/Data/ApplicationDbContext.cs
@@ -6,6 +6,21 @@ namespace TheWealthBlueprint.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.Property(e => e.FirstName)
+                      .IsRequired()
+                      .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                      .IsRequired()
+                      .HasMaxLength(50);
+            });
+        }
 
     }
 }
--- TheWealthBlueprint/Data/Migrations/20251029045615_AddedNameLength.Designer.cs
+++ TheWealthBlueprint/Data/Migrations/20251029045615_AddedNameLength.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheWealthBlueprint.Data;
 
@@ -11,9 +12,11 @@ using TheWealthBlueprint.Data;
 namespace TheWealthBlueprint.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251029045615_AddedNameLength")]
+    partial class AddedNameLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
